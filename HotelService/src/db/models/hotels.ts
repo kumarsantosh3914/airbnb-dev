171a28e7a744ec1efbba0ba830c1cc13290472ec
Santosh Kumar <santoshkumar15novmth@gmail.com>
2025-04-22 01:47:44 +0530

    Added the hotels model

---
+++ HotelService/src/db/models/hotels.ts
@@ -0,0 +1,55 @@
+import { InferAttributes, InferCreationAttributes, CreationOptional, Model } from "sequelize";
+import sequelize from './sequelize';
+
+class Hotel extends Model<InferAttributes<Hotel>,
+    InferCreationAttributes<Hotel> > {
+        declare id: CreationOptional<number>;
+        declare name: string;
+        declare address: string;
+        declare location: string;
+        declare createdAt: CreationOptional<Date>;
+        declare updatedAt: CreationOptional<Date>;
+        declare rating: number;
+        declare ratingCount: number;
+    }
+
+    Hotel.init({
+        id: {
+            type: "INTEGER",
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: "STRING",
+            allowNull: false
+        },
+        address: {
+            type: "STRING",
+            allowNull: false
+        },
+        location: {
+            type: "STRING",
+            allowNull: false
+        },
+        createdAt: {
+            type: "DATE",
+            defaultValue: new Date()
+        },
+        updatedAt: {
+            type: "DATE",
+            defaultValue: new Date()
+        },
+        rating: {
+            type: "FLOAT",
+            defaultValue: null
+        },
+        ratingCount: {
+            type: "INTEGER",
+            defaultValue: null
+        }
+    }, {
+    tableName: 'hotels',
+    sequelize: sequelize,
+    underscored: true, // createdAT --> created_at, updatedAt -> updated_at
+    timestamps: true, // createdAt, updatedAt
+});
