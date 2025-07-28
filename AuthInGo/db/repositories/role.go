5d0541752ad32b69c675c3b666ed7d7cd4b4d863
Santosh Kumar <santoshkumar15novmth@gmail.com>
2025-07-28 12:59:04 +0530

    Added role and permission repository

---
+++ AuthInGo/db/repositories/role.go
@@ -0,0 +1,128 @@
+package db
+
+import (
+	"AuthInGo/models"
+	"database/sql"
+)
+
+type RoleRepository interface {
+	GetRoleById(id int64) (*models.Role, error)
+	GetRoleByName(name string) (*models.Role, error)
+	GetAllRoles() ([]*models.Role, error)
+	CreateRole(name string, description string) (*models.Role, error)
+	DeleteRoleById(id int64) error
+	UpdateRole(id int64, name string, description string) (*models.Role, error)
+}
+
+type RoleRepositoryImp struct {
+	db *sql.DB
+}
+
+func NewRoleRepository(_db *sql.DB) RoleRepository {
+	return &RoleRepositoryImp{
+		db: _db,
+	}
+}
+
+func (r *RoleRepositoryImp) GetRoleById(id int64) (*models.Role, error) {
+	query := "SELECT id, name, description, created_at, updated_at FROM roles where id = ?"
+	row := r.db.QueryRow(query, id)
+
+	role := &models.Role{}
+	if err := row.Scan(&role.Id, &role.Name, &role.Description, &role.CreatedAt, &role.UpdatedAt); err != nil {
+		return nil, err
+	}
+	return role, nil
+}
+
+func (r *RoleRepositoryImp) GetRoleByName(name string) (*models.Role, error) {
+	query := "SELECT id, name, description, created_at, updated_at FROM roles WHERE name = ?"
+	row := r.db.QueryRow(query, name)
+
+	role := &models.Role{}
+	if err := row.Scan(&role.Id, &role.Name, &role.Description, &role.CreatedAt, &role.UpdatedAt); err != nil {
+		return nil, err
+	}
+	return role, nil
+}
+
+func (r *RoleRepositoryImp) GetAllRoles() ([]*models.Role, error) {
+	query := "SELECT id, name, description, created_at, updated_at FROM roles"
+
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var roles []*models.Role
+	for rows.Next() {
+		role := &models.Role{}
+		if err := rows.Scan(&role.Id, &role.Name, &role.Description, &role.CreatedAt, &role.UpdatedAt); err != nil {
+			return nil, err
+		}
+		roles = append(roles, role)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return roles, nil
+}
+
+func (r *RoleRepositoryImp) CreateRole(name string, description string) (*models.Role, error) {
+	query := "INSERT INTO roles (name, description, created_at, updated_at) VALUES (?, ?, NOW(), NOW())"
+	result, err := r.db.Exec(query, name, description)
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.Role{
+		Id:          id,
+		Name:        name,
+		Description: description,
+		CreatedAt:   "", // Will be set by the database
+		UpdatedAt:   "", // Will be set by the database
+	}, nil
+}
+
+func (r *RoleRepositoryImp) DeleteRoleById(id int64) error {
+	query := "DELETE FROM roles WHERE id = ?"
+	result, err := r.db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowAffected == 0 {
+		return sql.ErrNoRows
+	}
+
+	return nil
+}
+
+func (r *RoleRepositoryImp) UpdateRole(id int64, name string, description string) (*models.Role, error) {
+	query := "UPDATE roles SET name = ? description = ? updated_at = NOW() WHERE id = ?"
+	_, err := r.db.Exec(query, name, description, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.Role{
+		Id:          id,
+		Name:        name,
+		Description: description,
+		CreatedAt:   "",
+		UpdatedAt:   "",
+	}, nil
+}
